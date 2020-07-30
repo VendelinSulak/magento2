55c56f27672da9859dbac5819b7cfcc37322a981
Vendelin Sulak <vendelin@digitalwerk.agency>
2020-07-30 14:34:20 +0200

    Add env.php

---
+++ app/etc/env.php
@@ -0,0 +1,91 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '569e11108765974b9e3757118469962b'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'db',
+                'dbname' => 'db',
+                'username' => 'db',
+                'password' => 'db',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'a3a_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'a3a_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'magento-demo.ddev.site'
+    ],
+    'install' => [
+        'date' => 'Wed, 29 Jul 2020 16:20:06 +0000'
+    ],
+    'system' => [
+        'default' => [
+            'smile_elasticsuite_core_base_settings' => [
+                'es_client' => [
+                    'servers' => 'elasticsearch:9200',
+                    'enable_https_mode' => 0,
+                    'enable_http_auth' => 1
+                ]
+            ]
+        ]
+    ]
+];
